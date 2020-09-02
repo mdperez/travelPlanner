e996a7e3f323e41758ccfba398379c15b5fb4922
Miguel Dominguez Pérez <contacto@mdperez.es>
2020-09-02 12:30:45 +0200

    feat(functions): adds support for multiple destinations on trips

--- index.js
+++ index.js
@@ -28,6 +28,49 @@ const checkAddTripFields = () => {
   return validateTripName && validateDestination && validateStartDate && validateEndDate;
 }
 
+const checkAddDestinationFields = addDestinationSection => {
+  // TODO: add types validation and correct dates (end > start, and start > today)
+  const validateDestination = addDestinationSection.querySelector(".destinationName-input").value !== "";
+  const validateStartDate = addDestinationSection.querySelector(".destinationStartDate-input").value !== "";
+  const validateEndDate = addDestinationSection.querySelector(".destinationEndDate-input").value !== "";
+  return validateDestination && validateStartDate && validateEndDate;
+}
+
+const addDestination = trip => {
+  const ts = +new Date();
+  const destinationName = trip.querySelector(".destinationName-input").value;
+  const startDate = trip.querySelector(".destinationStartDate-input").value;
+  const endDate = trip.querySelector(".destinationEndDate-input").value;
+
+  const destinationContent = `
+    <div class="picture">
+      <img src="https://via.placeholder.com/150" />
+    </div>
+    <div class="data">
+      <h3 class="location">${destinationName}</h3>
+      <div class="dates">
+        <span class="date">${startDate}</span>
+        -
+        <span class="date">${endDate}</span>
+      </div>
+    </div>
+    <div class="buttons">
+      <div class="button button-warning removeDestination"><span class="icon-remove"></span> remove destination</div>
+      <div class="button button-success"><span class="icon-plus"></span> add transport</div>
+      <div class="button button-success"><span class="icon-plus"></span> add lodging</div>
+      <div class="button button-success"><span class="icon-plus"></span> add packing list</div>
+      <div class="button button-success"><span class="icon-plus"></span> add notes</div>
+    </div>
+  `;
+  
+  const destination = document.createElement("div");
+  destination.classList.add("destination");
+  destination.id = `destination-${ts}`;
+  destination.innerHTML = destinationContent;
+
+  trip.querySelector(".destinations").append(destination);
+};
+
 const addTrip = () => {
   const ts = +new Date();
   const tripName = document.querySelector("#tripName-input").value;
@@ -46,13 +89,13 @@ const addTrip = () => {
       <form class="wrapper">
         <div class="destinationData">
           <label>Destination: </label>
-          <input id="destinationName-input">
+          <input class="destinationName-input">
         </div>
         <div class="destinationDate">
           <label>start date: </label>
-          <input id="destinationStartDate-input">
+          <input class="destinationStartDate-input">
           <label>end date: </label>
-          <input id="destinationEndDate-input">
+          <input class="destinationEndDate-input">
         </div>
         <div class="buttons">
           <span class="button button-success icon-plus addDestination"></span>
@@ -113,13 +156,15 @@ const addTrip = () => {
     clearAddDestination(addDestinationSection);
   });
 
-  // trip.querySelector("#addDestination").addEventListener("click", (e) => {
-  //   if (checkAddDestinationFields()) {
-  //     addDestination();
-  //   } else {
-  //     alert("todo: validate fields");
-  //   }
-  // });
+  trip.querySelector(".addDestination").addEventListener("click", (e) => {
+    const currentTrip = e.currentTarget.closest(".trip");
+    const addDestinationSection = currentTrip.querySelector(".addDestination-section");
+    if (checkAddDestinationFields(addDestinationSection)) {
+      addDestination(currentTrip);
+    } else {
+      alert("todo: validate fields");
+    }
+  });
 
   document.querySelector("#addTrip-section").classList.remove("open");
   clearAddTrip();
