5a1e86ab2f0c29eddbf9106d1eaa26d302a1049b
Miguel Dominguez Pérez <contacto@mdperez.es>
2020-09-02 16:53:10 +0200

    refactor(content): working with webpack devserver

---
+++ webpack.dev.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  mode: 'development',
+  devtool: 'source-map',
+  stats: 'verbose',
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+    })
+  ]
+}
