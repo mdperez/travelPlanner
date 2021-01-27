7d8d14eab8b50fb529ed4f523921bf1be6e48c17
Miguel Dominguez Pérez <contacto@mdperez.es>
2021-01-27 19:16:15 +0100

    refactor(styles): split styles in multiple files

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,6 @@
+import './styles/reset.scss';
+import './styles/fonts.scss';
+import './styles/styles.scss';
+
 import {openAddTrip, clearAddTrip, closeAddTrip, checkAddTripFields, checkAddTrip, addTrip, removeTrip} from './js/trip.js';
 import { openAddDestination, clearAddDestination, closeAddDestination, checkAddDestinationFields, checkAddDestination, addDestination, removeDestination } from './js/destination.js';
-import './styles/styles.scss';
